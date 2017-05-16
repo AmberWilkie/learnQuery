fb30955bd17f8360453f5aa37bccf701d3a5ce4b
Amber <amber@amberwilkie.com>
2017-05-16 16:34:19 +0200

    Complete assignment 2

--- 02.CSS_manipulation/src/cssProp.js
+++ 02.CSS_manipulation/src/cssProp.js
@@ -1,5 +1,14 @@
-function cssProp() {
+function cssProp(element, property, value) {
   'use strict';
 
-  //code goes here
+  if (typeof(property) === 'string' && value !== undefined) {
+    element.setAttribute('style', `${property}: ${value}`)
+  } else if (typeof(property) === 'object') {
+    Object.assign(element.style, property);
+  } else {
+    // const style = element.style
+    // element.getAttribute(property.replace(/['"]+/g, ''))
+    return element.style[property];
+  }
+
 }
