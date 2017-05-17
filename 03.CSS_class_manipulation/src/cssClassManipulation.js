c23991855df49ed958eb0b5fc6b44622491812d0
Amber <amber@amberwilkie.com>
2017-05-17 13:35:47 +0200

    Return add, remove, toggle, has classes from cssClass fn call

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -1,19 +1,18 @@
 var cssClass = (function () {
   'use strict'
-  // The wrapping I'm seeing here seems to imply that we are going to run this when it loads.
-  // but we don't have access to element or className
-  console.log('base level of cssClass')
-  // console.log();
-  // function add (element, className) {
-  //   console.log('is add running? Then... ', element, className );
-  //   // element.addClass(className);
-  // };
-  // cssClass.add = add;
-  // return cssClass.add = add;
 
   return {
     add: function (element, classToAdd) {
-      element.className = classToAdd;
+      element.classList.add(classToAdd);
+    },
+    remove: function (element, classToRemove) {
+      element.classList.remove(classToRemove);
+    },
+    toggle: function (element, classToToggle) {
+      element.classList.toggle(classToToggle);
+    },
+    has: function(element, classToCheck) {
+      return element.classList.contains(classToCheck)
     }
   }
 })()
